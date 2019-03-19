5d02336c8c692ab7147ac17c0627202a9b13e524
Einar Sv <einarsve17@ru.is>
2019-03-19 18:19:40 +0000

    fixed imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ChooseNick from "./components/ChooseNick";
 import {Switch, Route} from 'react-router-dom';
+import { socket } from './services/socketService';
 
 class App extends React.Component {
     componentDidMount() {
